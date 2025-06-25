33419c08a3c3c0380fed0aecf5a78a8b50847815
BlackCoder56 <blackcoda56@gmail.com>
2025-06-25 08:40:41 +0300

    useTheString'sIndexOf()andSubstring()helperMethod

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
-﻿string message = "Find what is (inside the parentheses)";
+﻿// string message = "Find what is (inside the parentheses)";
 
-int opeingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
+// int opeingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
 
-Console.WriteLine(opeingPosition);
-Console.WriteLine(closingPosition);
+// // Console.WriteLine(opeingPosition);
+// // Console.WriteLine(closingPosition);
+
+// opeingPosition += 1;
+
+// int length = closingPosition - opeingPosition;
+
+// Console.WriteLine(message.Substring(opeingPosition, length));
+
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
