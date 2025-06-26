e6056f779117207aebc3cfef758aa75aa5376a43
BlackCoder56 <blackcoda56@gmail.com>
2025-06-26 23:13:43 +0300

    retrieve all the instances of substrings inside parentheses

--- Program.cs
+++ Program.cs
@@ -32,9 +32,25 @@
 // Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
 
 string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
+// int openingPosition = message.LastIndexOf('(');
 
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+// openingPosition += 1;
+// int closingPosition = message.LastIndexOf(')');
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+while (true)
+{
+    int opeingPosition = message.IndexOf('(');
+    if (opeingPosition == -1) break;
+
+    opeingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - opeingPosition;
+    Console.WriteLine(message.Substring(opeingPosition, length));
+
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1);
+}
+
